af91beb676ee1cf862aa9bd46184a1321d5fd648
scoboslor <scoboslor@gmail.com>
2022-05-03 14:13:43 +0200

    clases separadas en diferentes archivos

--- src/ende/Juego.java
+++ src/ende/Juego.java
@@ -5,19 +5,19 @@ package ende;
  */
 
 
-public class Juego 
+public class Juego
 {
     public static void main(String args[])
     {
         Jugador p1=new Jugador();
         Jugador p2=new Jugador();
-        boolean fin_de_juego=false;  
+        boolean fin_de_juego=false;
         Integer Rondasjugadas = 0;    // Número de rondas jugadas
         Integer EXITOS_jugador1=p1.éxitos;
         Integer EXITOS_jugador2=p2.éxitos;
         Integer EmPaTeS = 0;
         String opcion_JUGADOR1, opcion_JUGADOR2;
-        
+
         // Bucle de juego
         do
         {
@@ -27,12 +27,12 @@ public class Juego
             System.out.println("Jugador 1: " + opcion_JUGADOR1+"\t Jugador 1 - Partidas ganadas: " + EXITOS_jugador1);
             opcion_JUGADOR2 = p2.opcion_al_azar();
             System.out.println("Jugador 2: " + opcion_JUGADOR2+"\t Jugador 2 - Partidas ganadas: " + EXITOS_jugador2);
-            
+
             if((opcion_JUGADOR1.equals("piedra"))&&(opcion_JUGADOR2.equals("papel")))
             {
                 System.out.println("Jugador 2 GANA");
                 EXITOS_jugador2 = ++p2.éxitos;
-                
+
             }
             else if((opcion_JUGADOR1.equals("papel"))&&(opcion_JUGADOR2.equals("piedra")))
             {
@@ -73,40 +73,4 @@ public class Juego
             System.out.println();
         } while(fin_de_juego!=true);
     }
-}
-/**
- *
- */
-class Jugador{
-   
-    /**
-     * Escoge piedra, papel o tijera al azar
-     */
-    public String opcion_al_azar()
-    {
-        String opcion="";
-        Integer c = (int)(Math.random()*3);
-        switch(c){
-            case 0:
-            	opcion=("piedra");
-                break;
-            case 1:
-            	opcion=("papel");
-                break;
-            case 2:
-            	opcion=("tijeras");
-        }
-        return opcion;
-    }
-    public void setÉxitos() 
-    {
-        éxitos++;
-    }
-    public int getÉxitos() 
-    {
-        return(éxitos);
-    }
-    
-    int éxitos;      // número de partidas ganadas
-    int winTotal;
 }
---
+++ src/ende/Jugador.java
@@ -0,0 +1,37 @@
+package ende;
+
+/**
+ *
+ */
+class Jugador {
+
+    /**
+     * Escoge piedra, papel o tijera al azar
+     */
+    public String opcion_al_azar() {
+        String opcion = "";
+        Integer c = (int) (Math.random() * 3);
+        switch (c) {
+            case 0:
+                opcion = ("piedra");
+                break;
+            case 1:
+                opcion = ("papel");
+                break;
+            case 2:
+                opcion = ("tijeras");
+        }
+        return opcion;
+    }
+
+    public void setÉxitos() {
+        éxitos++;
+    }
+
+    public int getÉxitos() {
+        return (éxitos);
+    }
+
+    int éxitos;      // número de partidas ganadas
+    int winTotal;
+}
