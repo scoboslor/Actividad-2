2c1a913295af8235c0b02b707e393792cc5523ea
scoboslor <scoboslor@gmail.com>
2022-05-14 12:00:25 +0200

    romper lineas >100 caracteres

--- src/ende/Juego.java
+++ src/ende/Juego.java
@@ -21,9 +21,11 @@ public class Juego {
             System.out.println("***** Ronda: " + rondasJugadas + " *********************\n");
             System.out.println("Numero de empates: " + empates + "\n");
             OPCION_JUGADOR1 = p1.opcion_al_azar();
-            System.out.println("Jugador 1: " + OPCION_JUGADOR1 + "\t Jugador 1 - Partidas ganadas: " + EXITOS_JUGADOR1);
+            System.out.println("Jugador 1: " + OPCION_JUGADOR1 + "\t Jugador 1 - Partidas ganadas: "
+                    + EXITOS_JUGADOR1);
             OPCION_JUGADOR2 = p2.opcion_al_azar();
-            System.out.println("Jugador 2: " + OPCION_JUGADOR2 + "\t Jugador 2 - Partidas ganadas: " + EXITOS_JUGADOR2);
+            System.out.println("Jugador 2: " + OPCION_JUGADOR2 + "\t Jugador 2 - Partidas ganadas: "
+                    + EXITOS_JUGADOR2);
 
             if ((OPCION_JUGADOR1.equals("piedra")) && (OPCION_JUGADOR2.equals("papel"))) {
                 System.out.println("Jugador 2 GANA");
